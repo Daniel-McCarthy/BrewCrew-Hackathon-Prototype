6cb2cc63ae8504f6160c0f65ba316cc62f6c9fee
Daniel McCarthy <dan.willy.mccarthy@gmail.com>
2020-07-14 22:39:35 -0500

    Added functions to simulate entering info to sign up form
    Since we don't have time to implement a database and handle network requests/email handing/authentication of registration and login, we just want to demonstrate the idea.
    
    So I have added a function that displays typing in credentials into the signup form as an illustration.
    
    This will be expanded as we simulate log in by changing the login/sing up buttons, and show a way to get to the stream page.

--- Website Source/src/Components/sign-in.jsx
+++ Website Source/src/Components/sign-in.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import '../Styles/sign-in.css';
+import $ from 'jquery';
 
 export class SignIn extends React.Component {
     constructor(props) {
@@ -8,17 +9,90 @@ export class SignIn extends React.Component {
         }
     }
 
+    fakeRegistration = () => {
+        let nameEntryData = { inputText: 'New User', inputId: '#nameInput', overlayId: '#nameOverlay', overlayTextId: '#nameOverlayText'}
+        let emailEntryData = { inputText: 'newCustomer@gmail.com', inputId: '#emailInput', overlayId: '#emailOverlay', overlayTextId: '#emailOverlayText'}
+        let passwordEntryData = {inputText: '•••••••••', inputId: '#passwordInput', overlayId: '#passwordOverlay', overlayTextId: '#passwordOverlayText'}
+        let dataArray = [ nameEntryData, emailEntryData, passwordEntryData ];
+
+        this.clearPlaceholders();
+        this.simulateInputEntry(dataArray);
+    }
+
+    clearPlaceholders = () => {
+        this.setState({namePlaceholder: '', emailPlaceholder: '', passwordPlaceholder: ''})
+    }
+
+    simulateInputEntry = (dataArray) => {
+        if (dataArray.length === 0)
+            return;
+
+        let nextInput = dataArray[0];
+        let inputText = nextInput.inputText;
+        let inputID = nextInput.inputId;
+        let overlayID = nextInput.overlayId;
+        let overlayTextID = nextInput.overlayTextId;
+        
+        let inputElement = $(inputID);
+        inputElement.addClass('hideCursor');
+
+        let inputOverlay = $(overlayID);
+        inputOverlay.css('background-color', 'transparent');
+        
+        let inputWidth = inputElement.outerWidth();
+        let inputHeight = inputElement.outerHeight();
+
+        inputOverlay.css('background-color', 'none').css('vertical-align', 'middle');
+        inputOverlay.outerWidth(inputWidth).outerHeight(inputHeight);
+
+        let overlayText = $(overlayTextID);
+        overlayText.text(inputText.charAt(0));
+        let overlayTextHeight = overlayText.outerHeight();
+        let inputOverlayHeight = inputOverlay.outerHeight();
+        let padding = (inputOverlayHeight - overlayTextHeight) / 2;
+        overlayText.css('padding-top', padding).css('padding-bottom', padding).css('margin-top', 0).css('margin-bottom', 0);
+        
+        let inputPosition = inputElement.position();
+        inputOverlay.css('top', inputPosition.top).css('left', inputPosition.left);
+        inputOverlay.css('margin-top', inputElement.css('margin-top'))
+
+        let i = 1;
+        let length = inputText.length;
+        let timer = setInterval(
+            () => {
+                if (i >= length) {
+                    clearInterval(timer);
+                    dataArray.shift();
+                    this.simulateInputEntry(dataArray);
+                }
+
+                overlayText.text(overlayText.text()+ inputText.charAt(i));
+                i++;
+            }, 75
+        )
+    }
+
     render() {
         return (
             <div className={"signup-form"}>
                 <form action={"index.html"} method={"post"}>
                     <h1>Sign Up</h1>
-                    <input type="text" placeholder="Full Name" className="txtb"></input>
-                    <input type="email" placeholder="Email" className="txtb"></input>
-                    <input type="password" placeholder="Password" className="txtb"></input>
                     <input type="submit" value="Create Account" className="signup-btn"></input>
                     <a href="#">Already Have one ?</a>
+                    <input id='nameInput'     type="text"     placeholder="Full Name"   className={"txtb"} onClick={this.fakeRegistration}></input>
+                    <input id='emailInput'    type="email"    placeholder="Email"       className="txtb"></input>
+                    <input id='passwordInput' type="password" placeholder="Password"    className="txtb"></input>
                 </form>
+
+                <span id='nameOverlay' className='overlayDiv'>
+                    <h3 id='nameOverlayText'></h3>
+                </span>
+                <span id='emailOverlay' className='overlayDiv'>
+                    <h3 id='emailOverlayText'></h3>
+                </span>
+                <span id='passwordOverlay' className='overlayDiv'>
+                    <h3 id='passwordOverlayText'></h3>
+                </span>
             </div>
         )
     }
