f2488bbf4dd9cce384d6a1421ca4bebb02b68177
Daniel McCarthy <dan.willy.mccarthy@gmail.com>
2020-07-16 08:28:09 -0500

    Update Age Verification form to not accept incomplete input
    - Addresses not putting a full year, such as '1'
    -Addresses not putting a day  being valid
    -Addresses people 22 or being invalid based on month or days.

--- Website Source/src/Components/AgeVerification.jsx
+++ Website Source/src/Components/AgeVerification.jsx
@@ -17,14 +17,18 @@ export class AgeVerification extends React.Component {
 
     validateAge = () => {
         let givenAge = new Date(this.state.year, this.state.month, this.state.day);
+        let yearsOld = this.date.getFullYear() - givenAge.getFullYear();
+        
+        if (yearsOld > 21)
+            return true;
 
-        if (this.date.getFullYear() - givenAge.getFullYear() < 21)
+        if (yearsOld < 21)
             return false;
 
         if ((this.date.getMonth() + 1) < givenAge.getMonth())
             return false;
 
-        if (this.date.getDate() < givenAge.getDate())
+        if ((this.date.getDate() + 1) < givenAge.getDate())
             return false;
 
         return true;
@@ -89,12 +93,15 @@ export class AgeVerification extends React.Component {
                         <b className='headerText'>Age Verification</b><br/>
                         <a>You must be <b><i>at least 21</i></b> to access this website.</a><br/>
                         <a>Please enter your age below:</a><br/>
-                        <form>
-                            <input id='monthInput' onChange={this.onMonthInput}  placeholder="MM"></input>
-                            <input id='dayInput'   onChange={this.onDayInput}    placeholder="DD"></input>
-                            <input id='yearInput'  onChange={this.onYearInput}   placeholder="YYYY"></input>
+                        <form onSubmit={this.onSubmitClick}>
+                            <div className='buttonContainer'>
+                                <input id='monthInput' onChange={this.onMonthInput}  placeholder="MM"   pattern='[0-9]?[0-9]{1}'      title='Month must be 1 to 2 digits.'></input>
+                                <input id='dayInput'   onChange={this.onDayInput}    placeholder="DD"   pattern='[0-9]?[0-9]{1}'      title='Day must be 1 to 2 digits.'></input>
+                                <input id='yearInput'  onChange={this.onYearInput}   placeholder="YYYY" pattern='(19|20)[0-9]{2}' title='Year must be in 4 digit XXXX format'></input>
+                                {/* <input type="date"></input> */}
+                            </div>
+                            <input type="submit" value="Submit Age"/>
                         </form>
-                        <button onClick={this.onSubmitClick}>Submit Age</button>
                     </div>
                 </div>
             </div>
